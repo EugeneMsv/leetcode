dfa4559687b020099682c99e69c7af497f944b4b
Evgenii Moiseev <EugeneMsv@users.noreply.github.com>
2023-11-20 19:20:11 -0800

    add 3 sortings

--- src/main/java/sorting/n75SortColors/Solution.java
+++ src/main/java/sorting/n75SortColors/Solution.java
@@ -11,14 +11,14 @@ public class Solution {
 
   }
 
-  private void bubbleSort(int[] nums) {
+  private void bubbleSort(int[] nums){
 
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i; j < nums.length; j++) {
-        if (nums[j] < nums[i]) {
-          int temp = nums[i];
-          nums[i] = nums[j];
-          nums[j] = temp;
+    for(int i = 0; i < nums.length; i++){
+      for(int j = 0; j< nums.length -i-1; j++){
+        if(nums[j] > nums[j+1]){
+          int temp = nums[j+1];
+          nums[j+1] = nums[j];
+          nums[j] =  temp;
         }
       }
     }
