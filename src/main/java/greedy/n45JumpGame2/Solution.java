bd0ba1e31760f18b21e1ed0cb0a92298122a4d42
Evgenii Moiseev <EugeneMsv@users.noreply.github.com>
2024-02-23 18:26:21 -0800

    add 45

---
+++ src/main/java/greedy/n45JumpGame2/Solution.java
@@ -0,0 +1,18 @@
+package greedy.n45JumpGame2;
+
+public class Solution {
+
+  public int jump(int[] nums) {
+    int jumps = 0, curEnd = 0, curFarthest = 0;
+
+    for (int i = 0; i < nums.length - 1; i++) {
+      curFarthest = Math.max(curFarthest, i + nums[i]);
+      if (i == curEnd) {
+        jumps++;
+        curEnd = curFarthest;
+      }
+    }
+    return jumps;
+
+  }
+}
